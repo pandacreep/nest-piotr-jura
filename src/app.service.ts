4301aef8369ebf797f110b2a8dd8441258a891db
dmitriy.pak <dmt.alfa@gmail.com>
2025-09-27 15:38:00 +0500

    Building services with DI

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { LoggerService } from './logger/logger.service';
 
 @Injectable()
 export class AppService {
+  constructor(
+    private readonly loggerService: LoggerService
+  ) {}
+
   getHello(): string {
-    return 'Hello World!';
+    return this.loggerService.log('Hello!');
   }
 }
