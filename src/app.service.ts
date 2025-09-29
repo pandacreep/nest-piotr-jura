06926b32564dd2ca355143b94b1d976a7619f733
pandacreep <dmitriy.pak@mail.ru>
2025-09-29 13:08:41 +0500

    Creating Module

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,9 +3,7 @@ import { LoggerService } from './logger/logger.service';
 
 @Injectable()
 export class AppService {
-  constructor(
-    private readonly loggerService: LoggerService
-  ) {}
+  constructor(private readonly loggerService: LoggerService) {}
 
   getHello(): string {
     return this.loggerService.log('Hello!');
--- src/logger/logger.service.ts
+++ src/logger/logger.service.ts
@@ -3,11 +3,13 @@ import { MessageFormatterService } from 'src/message-formatter/message-formatter
 
 @Injectable()
 export class LoggerService {
-  constructor(private readonly messageFormatterService: MessageFormatterService) {}
+  constructor(
+    private readonly messageFormatterService: MessageFormatterService,
+  ) {}
 
   log(message: string): string {
     const formattedMessage = this.messageFormatterService.format(message);
     console.log(formattedMessage);
-	return formattedMessage;
+    return formattedMessage;
   }
 }
