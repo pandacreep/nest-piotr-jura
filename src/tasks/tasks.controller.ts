ddd3250bb9e62274610218c8e903210b84633d75
pandacreep <dmitriy.pak@mail.ru>
2025-10-02 13:04:17 +0500

    Fetching data in a service

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,17 +1,18 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { TasksService } from './tasks.service';
+import { ITask } from './task.model';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
 
   @Get()
-  findAll(): string[] {
-    return ['A', 'B'];
+  findAll(): ITask[] {
+    return this.tasksService.findAll();
   }
 
   @Get('/:id')
-  findOne(@Param('id') id: string): string {
-    return `The number is ${id}`;
+  findOne(@Param('id') id: string): ITask | undefined {
+    return this.tasksService.findOne(id);
   }
 }
