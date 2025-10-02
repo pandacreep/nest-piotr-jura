723903313cd145f5e10ef2f9090751b061f69b69
pandacreep <dmitriy.pak@mail.ru>
2025-10-02 12:55:54 +0500

    DTO

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
+import { ITask } from './task.model';
 
 @Injectable()
 export class TasksService {
-  private tasks = [];
+  private tasks: ITask[] = [];
 }
