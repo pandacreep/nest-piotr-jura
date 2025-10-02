6b856cae11797c2502bbce460b77e5712f85f913
pandacreep <dmitriy.pak@mail.ru>
2025-10-02 12:49:16 +0500

    Services

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,7 +1,10 @@
 import { Controller, Get, Param } from '@nestjs/common';
+import { TasksService } from './tasks.service';
 
 @Controller('tasks')
 export class TasksController {
+  constructor(private readonly tasksService: TasksService) {}
+
   @Get()
   findAll(): string[] {
     return ['A', 'B'];
