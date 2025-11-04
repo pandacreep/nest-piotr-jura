728cfee88a5508d878834ee02fe2cbcae759586c
pandacreep <dmitriy.pak@mail.ru>
2025-11-04 08:53:58 +0500

    DB configuration with TypeORM and environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,11 +8,12 @@ import { ConfigModule } from '@nestjs/config';
 import { appConfig } from './config/app.config';
 import { DummyService } from './dummy/dummy.service';
 import { appConfigSchema } from './config/config.types';
+import { typeOrmConfig } from './config/database.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      load: [appConfig],
+      load: [appConfig, typeOrmConfig],
       validationSchema: appConfigSchema,
       validationOptions: {
         // allowUnknown: false,
