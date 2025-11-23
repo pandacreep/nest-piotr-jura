6609860377f5141905cbe9f6f29806169260be40
pandacreep <dmitriy.pak@mail.ru>
2025-11-24 03:36:35 +0500

    Adding User entity & One-to-Many Explained

---
+++ src/users/user.entity.ts
@@ -0,0 +1,19 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
