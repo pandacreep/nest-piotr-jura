624543405d6b9faf8d82b267c692fcfdefe0a9ea
pandacreep <dmitriy.pak@mail.ru>
2025-11-25 08:57:14 +0500

    Adding Labels to our Tasks

--- src/tasks/task.entity.ts
+++ src/tasks/task.entity.ts
@@ -1,6 +1,7 @@
-import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 import { TaskStatus } from './task.model';
 import { User } from 'src/users/user.entity';
+import { TaskLabel } from './task-label.entity';
 
 @Entity()
 export class Task {
@@ -21,4 +22,7 @@ export class Task {
 
   @ManyToOne(() => User, (user) => user.tasks, { nullable: false })
   user: User;
+
+  @OneToMany(() => TaskLabel, (label) => label.id)
+  labels: TaskLabel[];
 }
