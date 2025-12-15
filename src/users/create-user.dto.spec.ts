528a7d4d91b36c920a6dd0543589b332f0623cc6
pandacreep <dmitriy.pak@mail.ru>
2025-12-15 08:29:43 +0500

    writing first unit test for DTO

--- src/users/create-user.dto.spec.ts
+++ src/users/create-user.dto.spec.ts
@@ -1,5 +1,21 @@
+import { validate } from 'class-validator';
+import { CreateUserDto } from './create-user.dto';
+
 describe('CreateUserDto', () => {
   it('ss', () => {
     expect(2).toBe(2);
+  });
+
+  it('should valicate complete valid data', async () => {
+    // 3xA
+    // Arrange
+    const dto = new CreateUserDto();
+    dto.email = 'test@test.com';
+    dto.name = 'Piotr';
+    dto.password = '123456';
+    // Act
+    const errors = await validate(dto);
+    // Assert
+    expect(errors.length).toBe(0);
   });
 });
