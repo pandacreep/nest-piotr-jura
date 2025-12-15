f838da99c588cd620e0cb0c4306ff65ccf4314c8
pandacreep <dmitriy.pak@mail.ru>
2025-12-15 08:44:36 +0500

    Adding detailed unit test cases

--- src/users/create-user.dto.spec.ts
+++ src/users/create-user.dto.spec.ts
@@ -2,20 +2,28 @@ import { validate } from 'class-validator';
 import { CreateUserDto } from './create-user.dto';
 
 describe('CreateUserDto', () => {
-  it('ss', () => {
-    expect(2).toBe(2);
-  });
+  let dto = new CreateUserDto();
 
-  it('should valicate complete valid data', async () => {
-    // 3xA
-    // Arrange
-    const dto = new CreateUserDto();
+  beforeEach(() => {
+    dto = new CreateUserDto();
     dto.email = 'test@test.com';
     dto.name = 'Piotr';
     dto.password = '123456';
-    // Act
+  });
+
+  it('should valicate complete valid data', async () => {
     const errors = await validate(dto);
-    // Assert
+
     expect(errors.length).toBe(0);
+  });
+
+  it('should fail on invalid email', async () => {
+    dto.email = 'test';
+
+    const errors = await validate(dto);
+    expect(errors.length).toBeGreaterThan(0);
+    console.log(errors);
+    expect(errors[0].property).toBe('email');
+    expect(errors[0].constraints).toHaveProperty('isEmail');
   });
 });
